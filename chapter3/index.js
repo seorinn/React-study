9d26c0759b58069eec5ffc83b287d2a25cfe8c4b
seorinn <seorin@ajou.ac.kr>
2023-06-21 02:09:37 +0900

    chapter3

---
+++ chapter3/index.js
@@ -0,0 +1,11 @@
+//console.log("index run");
+
+/*
+import { PI, getArea, getCircumference } from "./circle.js";
+
+console.log(PI, getArea(1), getCircumference(1));
+*/
+
+import circle from "./circle.js";
+
+console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
