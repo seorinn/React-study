0697a21fce8a732422e4c0e2ad6f224b08f77a97
seorinn <80616701+seorinn@users.noreply.github.com>
2023-06-25 14:59:54 +0900

    chapter6

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,17 +1,49 @@
 import './App.css';
 import Viewer from "./component/Viewer";
 import Controller from "./component/Controller";
-import {useState} from "react";
+import {useRef, useEffect, useState} from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+  const didMountRef=useRef(false);
+
+  useEffect(() => {
+    const intervalID = setInterval(() =>{
+      console.log("깜빡");
+    }, 1000);
+    return()=>{
+      console.log("clean up");
+      clearInterval(intervalID);
+    }
+    /*
+    if(!didMountRef.current){
+      didMountRef.current = true;
+      return;
+    }
+    else{
+      console.log("component updated")
+    }
+    */
+  });
+
+  useEffect(()=> {
+    console.log("component mount");
+  }, []);
+
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText}/>
+      </section>
       <section>
         <Viewer count = {count} />
       </section>
