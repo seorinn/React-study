1e7f333f026927f2e75982a796a294ef68f9138d
seorinn <seorin@ajou.ac.kr>
2023-07-09 00:09:16 +0900

    sort

--- project3/src/component/DiaryList.js
+++ project3/src/component/DiaryList.js
@@ -1,5 +1,7 @@
 import "./DiaryList.css";
 import Button from "./Button";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const sortOptionList = [
     {value: "latest", name: "최신순"},
@@ -7,11 +9,37 @@ const sortOptionList = [
 ]
 
 const DiaryList = ({data}) => {
+    const [sortType, setSortType] = useState("latest");
+    const [sortedData, setSortedData] = useState([]);
+
+    useEffect(() => {
+        const compare = (a, b) => {
+            if(sortType === "latest"){
+                return Number(b.date) - Number(a.date);
+            }
+            else {
+                return Number(a.date) - Number(b.date);
+            }
+        };
+        const copyList = JSON.parse(JSON.stringify(data));
+        copyList.sort(compare);
+        setSortedData(copyList);
+    }, [data, sortType]);
+    
+    const onChangeSortType = (e) => {
+        setSortType(e.target.value);
+    };
+    const navigate = useNavigate();
+
+    const onClickNew = () => {
+        navigate("/new");
+    };
+
     return(
         <div className="DiaryList">
             <div className="menu_wrapper">
                 <div className="left_col">
-                    <select>
+                    <select value={sortType} onChange={onChangeSortType}>
                         {sortOptionList.map((it, idx) => (
                             <option key={idx} value={it.value}>
                                 {it.name}
@@ -20,7 +48,10 @@ const DiaryList = ({data}) => {
                     </select>
                 </div>
                 <div className="right_col">
-                    <Button type={"positive"} text={"새 일기 쓰기"}/>
+                    <Button 
+                        type={"positive"} 
+                        text={"새 일기 쓰기"}
+                        onClick={onClickNew}/>
                 </div>
             </div>
         </div>
