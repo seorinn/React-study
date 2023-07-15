3d7818883fab18d67a9a532bf8805b5e63bed8de
seorinn <seorin@ajou.ac.kr>
2023-07-15 22:28:05 +0900

    Local Storage로 Data 저장

--- project3/src/App.js
+++ project3/src/App.js
@@ -10,37 +10,48 @@ import Edit from './pages/Edit';
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const mockData = [
-  {
-    id: "mock1",
-    date: new Date().getTime() -1,
-    content: "mock1",
-    emotionId: 1,
-  },
-  {
-    id: "mock2",
-    date: new Date().getTime() -2,
-    content: "mock2",
-    emotionId: 2,
-  },
-  {
-    id: "mock3",
-    date: new Date().getTime() -3,
-    content: "mock3",
-    emotionId: 3,
-  },
-]
+// const mockData = [
+//   {
+//     id: "mock1",
+//     date: new Date().getTime() -1,
+//     content: "mock1",
+//     emotionId: 1,
+//   },
+//   {
+//     id: "mock2",
+//     date: new Date().getTime() -2,
+//     content: "mock2",
+//     emotionId: 2,
+//   },
+//   {
+//     id: "mock3",
+//     date: new Date().getTime() -3,
+//     content: "mock3",
+//     emotionId: 3,
+//   },
+// ]
 
 function reducer (state, action) {
   switch(action.type){
-    case "CREATE":
-      return [action.data, ...state];
-    case "UPDATE":
-      return state.map((it) =>
+    case "CREATE":{
+      const newState = [action.data, ...state];
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
+    }
+    case "UPDATE":{
+      const newState = state.map((it) =>
         String(it.id) === String(action.data.id) ? { ...action.data } : it
       );
-    case "DELETE":
-      return state.filter((it) => String(it.id) !== String(action.targetId));
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
+    }
+    case "DELETE":{
+      const newState = state.filter(
+        (it) => String(it.id) !== String(action.targetId)
+      );
+      localStorage.setItem("diary", JSON.stringify(newState));
+      return newState;
+    }
     case "INIT":
       return action.data;
     default: 
@@ -54,9 +65,26 @@ function App() {
   const idRef = useRef(0);
   
   useEffect(() => {
+    // dispatch({
+    //   type: "INIT",
+    //   data: mockData,
+    // });
+    // setIsDataLoaded(true);
+    const rawData = localStorage.getItem("diary");
+    if(!rawData){
+      setIsDataLoaded(true);
+      return;
+    }
+    const localData = JSON.parse(rawData);
+    if(localData.length === 0){
+      setIsDataLoaded(true);
+      return;
+    }
+    localData.sort((a, b) => Number(b.id) - Number(a.id));
+    idRef.current = localData[0].id + 1;
     dispatch({
       type: "INIT",
-      data: mockData,
+      data: localData,
     });
     setIsDataLoaded(true);
   }, []);
